5a95a717635e44c9af109cd3ab7d9d79ba8b3397
Do Chau Long <dolong2110@gmail.com>
2022-08-07 23:13:05 +0700

    add multiply

--- polynomial.py
+++ polynomial.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import copy
+import math
 from typing import List, Union
 
 
@@ -52,11 +53,19 @@ class Polynomial:
 
     def val(self, value: Union[int, float]) -> float:
         """Returns the numerical result of evaluating the polynomial when x equals value"""
+        # naive - Time Complexity: O(N)
+        # res = 0
+        # i = self.len - 1
+        # while i >= 0:
+        #     res += self.coefficients[i] * value ** (self.len - i - 1)
+        #     i -= 1
+        #
+        # return float(res)
+
+        # Horner's method - Time Complexity: O(logN)
         res = 0
-        i = self.len - 1
-        while i >= 0:
-            res += self.coefficients[i] * value ** (self.len - i - 1)
-            i -= 1
+        for i in range(self.len):
+            res = res * value + self.coefficients[i]
 
         return float(res)
 
@@ -121,6 +130,28 @@ class Polynomial:
 
         return product
 
+    def roots(self) -> Union[List[Union[float, complex]], str]:
+        """Returns a list containing the root or roots of first or second order polynomials"""
+        if self.len == 2:
+            return [-self.coefficients[1] / self.coefficients[0]]
+        if self.len == 3:
+            a, b, c = self.coefficients[0], self.coefficients[1], self.coefficients[2]
+            delta = b ** 2 - 4 * a * c
+            if delta < 0:
+                x1 = (-b - complex(delta) ** 0.5) / (2 * a)
+                x2 = (-b + complex(delta) ** 0.5) / (2 * a)
+                return [x1, x2]
+            elif delta == 0:
+                return [-b / (2 * a)]
+            x1 = (-b - math.sqrt(delta)) / (2 * a)
+            x2 = (-b + math.sqrt(delta)) / (2 * a)
+
+            return [x1, x2]
+        if self.len == 1:
+            return "The equation has no roots"
+
+        return "Order is too high to find the roots"
+
     @staticmethod
     def _mul_constant(poly: List[Union[int, float]], const: Union[int, float]) \
             -> List[Union[int, float]]:
@@ -138,4 +169,8 @@ if __name__ == '__main__':
     print(p1.mul(p1))
     print(p1 * p1)
     print(p1 * p2 + p1)
-
+    print(p1.roots())
+    print(p2.roots())
+    p3 = Polynomial([3, 2, -1])
+    print(p3.roots())
+    print((p1 * p1).roots())
